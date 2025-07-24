89441cd1db6909da24b8b957696643f1321ab231
Darshanbreddy <brdarshan.2000@gmail.com>
2025-07-24 13:08:34 +0530

    added post method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -10,3 +11,8 @@ def root():
 @app.get("/posts")
 def get_post():
     return {"messsage: This is in get post"}
+
+@app.post("/create_post")
+def create_post(payload: dict= Body(...)):    #extracting the message by storing it in a dict called payload and priting it later
+    print(payload)
+    return {f"new post, title is {payload['title']} and Content is: {payload['content']}"}
